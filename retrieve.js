3952888e767f6664b4ed44589a60276a2286f4d8
kieranpark <kieran_afc_2k6@hotmail.com>
2015-11-23 15:29:16 +0000

    html update

--- retrieve.js
+++ retrieve.js
@@ -25,21 +25,20 @@ http.createServer(function(request, response) {
             response.write('Connection established to' + url +"\n");
             // Get the documents collection
             var collection = db.collection('users');
-            //We have a cursor now with our find criteria
-            var results = collection.find({name: 'modulus user'});
             //Lets iterate on the result
             results.each(function (err, result) {
-                if (err) {
-                    response.write(err);
-                } else {
-                    response.write('Fetched: ' + result.name + " : " + result.age + " : " + result.roles.toString() +'\n');
-                }
                 //if the result is null, there are no more results, it’s ok to close everything
                 if (result == null) {
                     response.end('Completed');
                     db.close();
                 }
-            });
+                if (err) {
+                    response.write(err);
+                } else {
+                    response.write('Fetched: ' + result.name + " : " + result.age + " : " + result.roles.toString() +'\n');
+                }
+
+        });
         }
     });
 
