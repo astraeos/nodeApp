98f02d1e1c8dcb708ea9a5205ac1ccf690c7c2b4
kieranpark <kieran_afc_2k6@hotmail.com>
2015-11-23 14:11:20 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+/**
+ * Created by kieranpark on 23/11/15.
+ */
+
+var http = require('http')
+var port = process.env.PORT || 1337;
+
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello World\n');
+}).listen(port);
