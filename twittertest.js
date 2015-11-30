ddf4dc2bb73cd7426c92494fcca006843b5c6ae2
kieranpark <kieran_afc_2k6@hotmail.com>
2015-11-30 14:50:40 +0000

    html update

--- twittertest.js
+++ twittertest.js
@@ -13,6 +13,10 @@ http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'application/json',
         'Access-Control-Allow-Origin' : '*' });
     client.get('search/tweets', {q: 'Lolcats'}, function(error, tweets){
-        response.end(JSON.stringify(tweets));
+        var json = [];
+        for (var i = 0; i<tweets.length; i++){
+            json.push({name: tweets.statuses[i].user.name, text: tweets.statuses[i].text});
+        }
+        response.end(JSON.stringify(json));
     });
 }).listen(port);
