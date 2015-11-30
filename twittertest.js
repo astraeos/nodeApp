2f2a4eb4e478c5384dbbe2258cf335ce87d873d0
kieranpark <kieran_afc_2k6@hotmail.com>
2015-11-30 14:31:46 +0000

    html update

--- twittertest.js
+++ twittertest.js
@@ -12,7 +12,7 @@ var client = new Twitter({
 http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'application/json',
         'Access-Control-Allow-Origin' : '*' });
-    client.get('search/tweets', {q: 'PeopleThatLookLikePeter'}, function(error, tweets){
+    client.get('search/tweets', {q: '#PeopleThatLookLikePeter'}, function(error, tweets){
         response.end(JSON.stringify(tweets));
     });
 }).listen(port);
