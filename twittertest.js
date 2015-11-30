4c0d9201348a7247a8765007fa2c2a40095d2439
kieranpark <kieran_afc_2k6@hotmail.com>
2015-11-30 14:56:18 +0000

    html update

--- twittertest.js
+++ twittertest.js
@@ -12,7 +12,7 @@ var client = new Twitter({
 http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'application/json',
         'Access-Control-Allow-Origin' : '*' });
-    client.get('search/tweets', {q: '@notkleeshay'}, function(error, tweets){
+    client.get('search/tweets', {q: '@NigelFarage'}, function(error, tweets){
         var json = [];
         for (var i =0; i< tweets.statuses.length ; i++)
         {
